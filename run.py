0df5467b6a57138aa8e8b201f8a967c8f1970a2e
Sam <unclesaamm@gmail.com>
2014-07-10 03:41:01 -0500

    test code

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from eatsafeapp import eatsafeapp
+import sys, os, inspect
 
 if __name__ == '__main__':
+    print os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
     eatsafeapp.debug = True
     eatsafeapp.run()

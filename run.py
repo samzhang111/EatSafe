a0d49542bcc79ffafc2b964e19d0164894712700
Sam <unclesaamm@gmail.com>
2014-07-10 04:54:33 -0500

    revert

--- run.py
+++ run.py
@@ -1,10 +1,5 @@
 from eatsafeapp import eatsafeapp
-import psycopg2
-import sys, os
 
 if __name__ == '__main__':
-    esdb = os.environ['EATSAFE_DATABASE_PATH']
-    print esdb
-    conn = psycopg2.connect(database='eatsafe')
     eatsafeapp.debug = True
     eatsafeapp.run()

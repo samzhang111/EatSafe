01218356c59e44ea28e1bee6108b7c6b8148ed12
Sam <unclesaamm@gmail.com>
2014-07-10 05:04:35 -0500

    psycopg2 debug

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
 from eatsafeapp import eatsafeapp
+import sys, os, psycopg2
 
 if __name__ == '__main__':
+    database = os.environ['EATSAFE_DB']
+    user = os.environ['EATSAFE_USER']
+    pw = os.environ['EATSAFE_PW']
+
+    db_str = 'dbname={db} host=127.0.0.1 user={user} pw={pw}'.format(
+            db=database,
+            user=user,
+            pw=pw)
+
+    conn = psycopg2.connect(db_str)
+    print conn.status
+
     eatsafeapp.debug = True
     eatsafeapp.run()

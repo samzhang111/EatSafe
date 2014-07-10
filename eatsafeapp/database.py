c8e6856e25c62253ab253a83320dfb9c29b1e120
Sam <unclesaamm@gmail.com>
2014-07-10 06:32:21 -0500

    changed env variable name

--- eatsafeapp/database.py
+++ eatsafeapp/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-db_path = os.environ['EATSAFE_DATABASE_PATH']
+db_path = os.environ['DATABASE_URL']
 
 engine = create_engine(db_path)
 Session = scoped_session(sessionmaker(bind=engine))

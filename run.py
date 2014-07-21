d084c073d1b08fd3c9d3d26560553028c525299d
Sam <unclesaamm@gmail.com>
2014-07-21 04:46:14 -0500

    proxyfix

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from eatsafeapp import eatsafeapp
+from werkzeug.contrib.fixers import ProxyFix
+
+eatsafeapp.wsgi_app = ProxyFix(eatsafeapp.wsgi_app)
 
 if __name__ == '__main__':
-
     eatsafeapp.debug = True
     eatsafeapp.run()

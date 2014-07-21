807c8e64703510ec50d3be140cffd57b4c685e8c
Sam <unclesaamm@gmail.com>
2014-07-21 03:39:09 -0500

    Database migration to Flask-SQLAlchemy
    Fixed several bugs, such as 1) Native Foods showing more inspections
    than there were, and 2) Inspections detail page not formatted in JSON.
    
    Also made large architectural change by transitioning to
    Flask-SQLAlchemy and initiating the use of migrations.

--- eatsafeapp/database.py
+++ eatsafeapp/database.py
@@ -1,10 +1,6 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from eatsafeapp import eatsafeapp
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
-db_path = os.environ['DATABASE_URL']
-
-engine = create_engine(db_path)
-Session = scoped_session(sessionmaker(bind=engine))
-session = Session()
+eatsafeapp.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(eatsafeapp)
